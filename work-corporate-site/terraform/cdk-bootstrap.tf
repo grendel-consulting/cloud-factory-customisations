6f63b523f529052b22bdf4fd20b24efa3334340a
James Ramirez <james.ramirez@grendel-consulting.com>
2023-05-03 05:33:44 +0100

    feat: bootstrap the CDK in the targeted workloads

---
+++ work-corporate-site/terraform/cdk-bootstrap.tf
@@ -0,0 +1,7 @@
+module "cdk_bootstrap" {
+  source = "github.com/grendel-consulting/terraform-aws-cdk_bootstrap?ref=v17"
+
+  file_assets_bucket_kms_key_id = "AWS_MANAGED"
+  qualifier                     = "v17-jerboa" # Unique and searchable, but meaningless
+  trusted_accounts              = local.control-plane
+}
