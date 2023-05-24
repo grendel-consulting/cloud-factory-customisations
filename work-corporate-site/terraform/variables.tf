b350563ff9b5628bdf1d01e99b2fd8c5ebeb01bd
James Ramirez <james.ramirez@grendel-consulting.com>
2023-05-24 06:06:12 +0100

    feat: create barebones pipeline

--- work-corporate-site/terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-# locals {
-#   github_owner = "grendel-consulting"
-#   repositories = {
-#     "grendel-consulting-com" = {
-#       github_repo = "www.grendel-consulting.com"
-#     }
-#   }
-# }
