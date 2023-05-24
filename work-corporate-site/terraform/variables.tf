b97b6d48e4e93094d473b993705cd43b5babb460
James Ramirez <james.ramirez@grendel-consulting.com>
2023-05-24 05:45:55 +0100

    chore: prepare to move target account

--- work-corporate-site/terraform/variables.tf
+++ work-corporate-site/terraform/variables.tf
@@ -1,8 +1,8 @@
-locals {
-  github_owner = "grendel-consulting"
-  repositories = {
-    "grendel-consulting-com" = {
-      github_repo = "www.grendel-consulting.com"
-    }
-  }
-}
+# locals {
+#   github_owner = "grendel-consulting"
+#   repositories = {
+#     "grendel-consulting-com" = {
+#       github_repo = "www.grendel-consulting.com"
+#     }
+#   }
+# }
