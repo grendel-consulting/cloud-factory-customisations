a2aa278db8df8772d1bc89bf025ab5166ab34dd4
James Ramirez <james.ramirez@grendel-consulting.com>
2023-05-25 06:21:54 +0100

    build: uplift CodeBuild to v7.0

--- infra-control-plane/terraform/modules/pipeline/codebuild.tf
+++ infra-control-plane/terraform/modules/pipeline/codebuild.tf
@@ -13,7 +13,7 @@ resource "aws_codebuild_project" "build" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
+    image                       = "aws/codebuild/standard:7.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
   }
